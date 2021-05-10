c35f8e4603015f1e8060f89594005a3dd1949027
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-05-10 11:49:44 -0400

    update functions

--- functions.js
+++ functions.js
@@ -1,39 +1,79 @@
-const returnEmptyString = function () {};
+const returnEmptyString = function () {
+    return "";
+};
 
-const returnZeroNumber = function () {};
+const returnZeroNumber = function () {
+    return 0;
+};
 
-const returnEmptyArray = function () {};
+const returnEmptyArray = function () {
+    return [];
+};
 
-const returnEmptyObject = function () {};
+const returnEmptyObject = function () {
+    return {};
+};
 
-const returnAbcString = function (value) {};
+const returnAbcString = function (value) {
+    return "abc123";
+};
 
-const returnSumOfTen = function (arg1, arg2) {};
+const returnSumOfTen = function (arg1, arg2) {
+    return arg1 + arg2;
+};
 
-const subtraction = function (arg1, arg2) {};
+const subtraction = function (arg1, arg2) {
+    return arg1 - arg2;
+};
 
-const multiplication = function (arg1, arg2) {};
+const multiplication = function (arg1, arg2) {
+    return arg1 * arg2;
+};
 
-const division = function (arg1, arg2) {};
+const division = function (arg1, arg2) {
+    return arg1 / arg2
+};
 
-const returnArray = function (arg1) {};
+const returnArray = function (arg1) {
+    return arg1
+};
 
-const returnFirstIndex = function (arg1) {};
+const returnFirstIndex = function (arg1) {
+    return arg1[0];
+};
 
-const returnSecondIndex = function (arg1) {};
+const returnSecondIndex = function (arg1) {
+    return arg1[1];
+};
 
-const returnArrayLength = function (arg1) {};
+const returnArrayLength = function (arg1) {
+    return arg1[0,1,2]
+};
 
-const arraySum = function (arg1) {};
+const arraySum = function (arg1) {
+    return arg1
+};
 
-const arraySubtraction = function (arg1) {};
+const arraySubtraction = function (arg1) {
 
-const multiplicationArray = function (arg1) {};
+};
 
-const divisionArray = function (arg1) {};
+const multiplicationArray = function (arg1) {
 
-const oddArray = function (arg1) {};
+};
 
-const evenArray = function (arg1) {};
+const divisionArray = function (arg1) {
 
-const returnFruits = function (arg1) {};
+};
+
+const oddArray = function (arg1) {
+
+};
+
+const evenArray = function (arg1) {
+
+};
+
+const returnFruits = function (arg1) {
+    
+};
